af04d6cac5a7eb6709b0b62101a6f1e820580747
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-12 01:44:00 +0000

    Testing Fri Sep 12 2025 01:44:00 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex04/student/sphere.py
+++ chapter2/ex04/student/sphere.py
@@ -1,1 +1,13 @@
 # Write your program here
+import math
+radius = float(input("Enter the sphere's radius: "))
+diameter = 2 * radius
+circumference = diameter * math.pi
+surfaceArea = 4 * math.pi * radius * radius
+volume = (4/3) * math.pi * radius ** 3
+print("Diameter: ", diameter)
+print("Circumference: ", circumference)
+print("Surface area: ", surfaceArea)
+print("Volume: ", volume)
+
+
