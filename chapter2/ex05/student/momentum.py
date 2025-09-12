dfd83828e9cdba57e1715c87efc3d0e5cf13faaa
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-12 02:05:44 +0000

    Testing Fri Sep 12 2025 02:05:44 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex05/student/momentum.py
+++ chapter2/ex05/student/momentum.py
@@ -1,5 +1,5 @@
 # Write your program here
 mass = float(input("Enter the object's mass: "))
 velocity = float(input("Enter the object's velocity: "))
-momentum = mass * velocity
+momentum = mass * velocit
 print("The object's momentum is", momentum)
