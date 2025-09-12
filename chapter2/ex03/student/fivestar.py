5091d05b56ddf97186ef300529fcd46d89f9fd5b
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-12 01:14:22 +0000

    Testing Fri Sep 12 2025 01:14:22 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/fivestar.py
+++ chapter2/ex03/student/fivestar.py
@@ -1,1 +1,5 @@
 # Write your program here
+newVideos = float(input("Enter the number of new videos: "))
+oldies = float(input("Enter the number of oldies: "))
+totalCost = newVideos * 3 + oldies * 2
+print("The total cost is $", totalCost)
