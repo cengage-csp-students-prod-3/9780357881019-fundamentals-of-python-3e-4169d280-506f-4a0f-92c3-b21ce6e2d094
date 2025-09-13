8536afcec442c6dd6ba2a0c83e31a7a894604d04
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-13 23:41:42 +0000

    Testing Sat Sep 13 2025 23:41:42 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex08/student/lightyear.py
+++ chapter2/ex08/student/lightyear.py
@@ -1,1 +1,4 @@
 # Write your program here
+years = float(input("Enter the number of years: "))
+lightYears = years * 9.461 * 10 ** 15
+print("Light travels", int(lightYears), "meters in", int(years), "years.")
