eee0deb90cf314072f920ab328ef752167e61236
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-13 23:19:14 +0000

    Testing Sat Sep 13 2025 23:19:14 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/momentum.py
+++ chapter2/ex06/student/momentum.py
@@ -1,1 +1,7 @@
 # Write your program here
+mass = float(input("Enter object's mass: "))
+velocity = float(input("Enter the object's velocity: "))
+momentum = mass * velocity
+kineticEnergy = (1/2) * mass * velocity ** 2
+print("The object's momentum is", str(momentum))
+print("The object's kinetic energy is", str(kineticEnergy))
