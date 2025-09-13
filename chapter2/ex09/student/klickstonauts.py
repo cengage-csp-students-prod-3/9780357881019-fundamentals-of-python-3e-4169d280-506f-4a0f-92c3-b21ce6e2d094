fd5d1ec0d7337458e1ffabc9d6a00778292d6d25
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-13 23:58:12 +0000

    Testing Sat Sep 13 2025 23:58:12 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex09/student/klickstonauts.py
+++ chapter2/ex09/student/klickstonauts.py
@@ -1,1 +1,4 @@
 # Write your program here
+kilometers = float(input("Enter the number of kilometers: "))
+nauticalMiles = (kilometers * 90 * 60) / 10000
+print("The number of nautical miles is", nauticalMiles)
