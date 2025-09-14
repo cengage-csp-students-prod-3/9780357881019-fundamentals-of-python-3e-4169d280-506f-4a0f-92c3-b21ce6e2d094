9dbc00fab5f9978dca5e40fe714c36a60f583874
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-14 00:12:24 +0000

    Testing Sun Sep 14 2025 00:12:24 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex10/student/employeepay.py
+++ chapter2/ex10/student/employeepay.py
@@ -1,1 +1,7 @@
 # Write your program here
+wage = float(input("Enter the wage: "))
+regHours = float(input("Enter the regular hours: "))
+overHours = float(input("Enter the overtime hours: "))
+overTime = overHours * wage * 1.5
+totalWeeklyPay = wage * regHours + overTime
+print("The total weekly pay is $" + str(totalWeeklyPay))
