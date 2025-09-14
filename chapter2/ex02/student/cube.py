098179e32aa848d7e975f4e032bd00433da23136
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-14 00:23:39 +0000

    Testing Sun Sep 14 2025 00:23:38 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/cube.py
+++ chapter2/ex02/student/cube.py
@@ -1,4 +1,4 @@
 # Write your program here
 cubeEdge = float(input("Enter the cube's edge: "))
-surfaceArea = cubeEdge ** 2 * 6
+surfaceArea = 6 * cubeEdge ** 2
 print("The surface area is", surfaceArea, "square units.")
