7caaf268a99bbf23c66143f7a4fec10041a1100d
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-18 02:46:35 +0000

    Testing Thu Sep 18 2025 02:46:35 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex02/student/right.py
+++ chapter3/ex02/student/right.py
@@ -1,2 +1,13 @@
 # Write your program here
-print('Hello')
+side1 = float(input("Enter the first side: "))
+side2 = float(input("Enter the second side: "))
+side3 = float(input("Enter the third side: "))
+square1 = side1 ** 2
+square2 = side2 ** 2
+square3 = side3 ** 2
+if square1 + square2 == square3 or \
+square2 + square3 == square1 or \
+square1 + square3 == square2:
+    print("The triangle is a right triangle.")
+else:
+    print("The triangle is not a right triangle.")
