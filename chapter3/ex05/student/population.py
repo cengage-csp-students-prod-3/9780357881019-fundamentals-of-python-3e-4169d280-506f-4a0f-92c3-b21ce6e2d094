859b83b4e6debf5e7833f929c0d3814dbd5422a0
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-21 23:41:57 +0000

    Testing Sun Sep 21 2025 23:41:57 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex05/student/population.py
+++ chapter3/ex05/student/population.py
@@ -1,1 +1,12 @@
 # Write your program here
+number = int(input("Enter the initial number of organisms: "))
+rate = float(input("Enter the rate of growth [a real number > 1]: "))
+cycleHours = int(input("Enter the number of hours to achieve the rate of growth: "))
+totalHours = int(input("Enter the total hours of growth: "))
+
+cycles = totalHours // cycleHours
+
+for eachPass in range(cycles):
+    number = number * rate
+
+print("The total population is", int(number))
