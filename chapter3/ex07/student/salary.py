64625330b78a8cf03de8494521dc96487a8ac7d3
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-22 00:33:39 +0000

    Testing Mon Sep 22 2025 00:33:39 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex07/student/salary.py
+++ chapter3/ex07/student/salary.py
@@ -1,1 +1,13 @@
 # Write your program here
+startSalary = float(input("Enter the starting salary: $"))
+increase = float(input("Enter the annual % increase: "))
+years = int(input("Enter the number of years: "))
+
+print("\nYear   Salary")
+print("-------------")
+
+salary = startSalary
+
+for year in range(1, years + 1):
+    print(f"{year:2}    {salary:.2f}")
+    salary *= (1 + increase / 100)
