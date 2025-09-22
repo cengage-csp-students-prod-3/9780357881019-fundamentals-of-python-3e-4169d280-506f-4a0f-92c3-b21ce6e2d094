062cd922783fea38d5919453035bd37f00e0fac7
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-22 01:49:53 +0000

    Testing Mon Sep 22 2025 01:49:53 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex08/student/gcd.py
+++ chapter3/ex08/student/gcd.py
@@ -1,1 +1,12 @@
 # Write your program here
+smallerNum = int(input("Enter the smaller number: "))
+largerNum = int(input("Enter the larger number: "))
+
+if smallerNum > largerNum:
+    smallerNum, largerNum = largerNum, smallerNum
+
+while smallerNum != 0:
+    remainder = largerNum % smallerNum
+    largerNum, smallerNum = smallerNum, remainder
+
+print("The greatest common divisor is", largerNum)
