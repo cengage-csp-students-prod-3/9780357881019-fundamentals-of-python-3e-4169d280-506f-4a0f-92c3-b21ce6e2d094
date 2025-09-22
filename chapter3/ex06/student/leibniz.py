a991aab578b8817fb05a5ffa433dd7bb5d5bff0f
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-22 00:20:38 +0000

    Testing Mon Sep 22 2025 00:20:37 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex06/student/leibniz.py
+++ chapter3/ex06/student/leibniz.py
@@ -1,1 +1,12 @@
 # Write your program here
+
+iterations = int(input("Enter the number of iterations: "))
+piApproximation = 0
+
+
+for i in range(iterations):
+    piApproximation += (-1) ** i / (2 * i + 1)
+
+piApproximation *= 4
+
+print("The approximation of pi is", piApproximation)
