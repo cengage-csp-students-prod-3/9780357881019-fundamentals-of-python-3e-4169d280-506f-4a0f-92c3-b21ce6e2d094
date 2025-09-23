64925cf7a8c0ce38ac5d0c8429b7bfeca3129584
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-23 15:07:45 +0000

    Testing Tue Sep 23 2025 15:07:45 GMT+0000 (Coordinated Universal Time)

--- chapter3/dex01/student/computeAverage.py
+++ chapter3/dex01/student/computeAverage.py
@@ -1,8 +1,12 @@
 count = 1
 total = 0
-while count < 10:
+while count <= 10:
     score = int(input("Enter test score number " + str(count) + ": "))
     total = total + score
-    count = count + 1
-average = total / count
+    if count != 10:
+        count = count + 1
+    else:
+        break
+
+average = total / (count)
 print("The average test score is", average)
