3b50a07e9d9990ae06fea6fe43e10a164a6e0940
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-25 15:09:08 +0000

    Testing Thu Sep 25 2025 15:09:08 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex09/student/numberlines.py
+++ chapter4/ex09/student/numberlines.py
@@ -1,1 +1,13 @@
 # Write your program here
+inputFileName = input("Enter the input file name: ")
+outputFileName = input("Enter the output file name: ")
+
+inputFile = open(inputFileName, 'r')
+outputFile = open(outputFileName, 'w')
+
+lineNumber = 0
+for line in inputFile:
+    lineNumber += 1
+    outputFile.write("%4d> %s" % (lineNumber, line))
+
+outputFile.close()
