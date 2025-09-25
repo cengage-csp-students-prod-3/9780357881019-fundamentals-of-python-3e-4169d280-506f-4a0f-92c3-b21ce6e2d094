8c4c727f32c3cbfefc6496128272be42c26b185e
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-25 02:09:14 +0000

    Testing Thu Sep 25 2025 02:09:14 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex02/student/decrypt.py
+++ chapter4/ex02/student/decrypt.py
@@ -1,1 +1,13 @@
 # Write your program here
+code = input("Enter the coded text: ")
+distance = int(input("Enter the distance value: "))
+plainText = ""
+
+for ch in code:
+    ordValue = ord(ch)
+    cipherValue = ordValue - distance
+    if cipherValue < 0:
+        cipherValue = 127 - (distance - (1 - ordValue))
+    plainText += chr(cipherValue)
+
+print(plainText)
