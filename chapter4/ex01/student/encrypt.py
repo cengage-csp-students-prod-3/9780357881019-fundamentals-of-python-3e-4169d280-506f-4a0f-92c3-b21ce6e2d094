84cc44b099ea5d8d46350cdbdc5d39b60cf046fd
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-25 01:59:24 +0000

    Testing Thu Sep 25 2025 01:59:24 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex01/student/encrypt.py
+++ chapter4/ex01/student/encrypt.py
@@ -1,1 +1,13 @@
 # Write your program here
+plainText = input("Enter a message: ")
+distance = int(input("Enter the distance vaule: "))
+code = ""
+
+for char in plainText:
+    ordValue = ord(char)
+    cipherValue = ordValue + distance
+    if cipherValue > 127:
+        cipherValue = distance - (127 - ordValue + 1)
+    code += chr(cipherValue)
+
+print(code)
