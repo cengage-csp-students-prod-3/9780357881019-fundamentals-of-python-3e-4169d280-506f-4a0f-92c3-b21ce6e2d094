7d697761ff76efe7c7b9189290d45bb2b89f5701
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-28 23:29:53 +0000

    Testing Sun Sep 28 2025 23:29:53 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex06/student/encrypt.py
+++ chapter4/ex06/student/encrypt.py
@@ -1,1 +1,20 @@
 # Write your program here
+plainText = input("Enter a message: ")
+
+code = ""
+
+for ch in plainText:
+    ordValue = ord(ch) + 1
+
+    binaryString = ""
+    while ordValue > 0:
+        remainder = ordValue % 2
+        ordValue = ordValue // 2
+        binaryString = str(remainder) + binaryString
+
+    if len(binaryString) > 1:
+        binaryString = binaryString[1:] + binaryString[0]
+
+    code += binaryString + " "
+
+print(code)
