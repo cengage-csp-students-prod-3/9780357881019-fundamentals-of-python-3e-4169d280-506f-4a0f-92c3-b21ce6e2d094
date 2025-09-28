dcebb317aebe35c6078e49846bbe0c320c325ebb
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-28 23:49:19 +0000

    Testing Sun Sep 28 2025 23:49:19 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex07/student/decrypt.py
+++ chapter4/ex07/student/decrypt.py
@@ -1,1 +1,21 @@
 # Write your program here
+code = input("Enter the coded text: ")
+
+wordList = code.split()
+plainText = ""
+
+for word in wordList:
+
+    word = word[-1] + word[:-1]
+
+    decimal = 0
+    exponent = len(word) - 1
+    for digit in word:
+        decimal = decimal + int(digit) * 2**exponent
+        exponent = exponent - 1
+
+    decimal -= 1
+
+    plainText += chr(decimal)
+
+print(plainText)
