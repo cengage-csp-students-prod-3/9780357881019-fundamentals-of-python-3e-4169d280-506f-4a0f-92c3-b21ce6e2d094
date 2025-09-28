7877dcc1c224a48cb1005741982c290deda1edc0
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-28 23:59:28 +0000

    Testing Sun Sep 28 2025 23:59:28 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex08/student/copyfile.py
+++ chapter4/ex08/student/copyfile.py
@@ -1,1 +1,11 @@
 # Write your program here
+
+inputFileName = input("Enter the input file name: ")
+outputFileName = input("Enter the output file name: ")
+
+inputFile = open(inputFileName, 'r')
+text = inputFile.read()
+
+outFile = open(outputFileName, 'w')
+outFile.write(text)
+outFile.close()
