1b5818a2264a875540f5c64193bce33357750770
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-28 21:47:40 +0000

    Testing Sun Sep 28 2025 21:47:40 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/decrypt.py
+++ chapter4/ex03/student/decrypt.py
@@ -1,1 +1,21 @@
 # Write your program here
+inputFileName = input("Enter the input file name: ")
+outputFileName = input("Enter the output file name: ")
+distance = int(input("Enter the distance value: "))
+
+inputFile = open(inputFileName, 'r')
+code = inputFile.read()
+
+outFile = open(outputFileName, 'w')
+
+plainText = ''
+for ch in code:
+    ordValue = ord(ch)
+    cipherValue = ordValue - distance
+    if cipherValue < 0:
+        cipherValue = 127 - \
+                        (distance - (1 - ordValue))
+    plainText += chr(cipherValue)
+
+outFile.write(plainText)
+outFile.close()
--- chapter4/ex03/student/encrypt.py
+++ chapter4/ex03/student/encrypt.py
@@ -1,1 +1,20 @@
 # Write your program here
+inputFileName = input("Enter the input file name: ")
+outputFileName = input("Enter the output file name: ")
+distance = int(input("Enter the distance value: "))
+
+inputFile = open(inputFileName, 'r')
+plainText = inputFile.read()
+
+outputFile = open(outputFileName, 'w')
+
+code = ''
+for ch in plainText:
+    ordValue = ord(ch)
+    cipherValue = ordValue + distance
+    if cipherValue > 127:
+        cipherValue = distance - (127 - ordValue + 1)
+    code += chr(cipherValue)
+
+outputFile.write(plainText)
+outputFile.close()
