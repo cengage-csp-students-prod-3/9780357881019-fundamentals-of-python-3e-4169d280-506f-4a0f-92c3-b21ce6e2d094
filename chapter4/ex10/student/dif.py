ab30f1630bcc5ed2c0d5fd14cd86748fd04c40b7
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-09-30 01:52:12 +0000

    Testing Tue Sep 30 2025 01:52:12 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex10/student/dif.py
+++ chapter4/ex10/student/dif.py
@@ -1,1 +1,20 @@
 # Write your program here
+fileName1 = input("Enter the first file name: ")
+fileName2 = input("Enter the second file name: ")
+
+inputFile1 = open(fileName1, 'r')
+inputFile2 = open(fileName2, 'r')
+
+while True:
+
+    line1 = inputFile1.readline()
+    line2 = inputFile2.readline()
+
+    if line1 == "" and line2 == "":
+        print("Yes")
+        break
+    elif line1 != line2:
+        print("No")
+        print(line1)
+        print(line2)
+        break
