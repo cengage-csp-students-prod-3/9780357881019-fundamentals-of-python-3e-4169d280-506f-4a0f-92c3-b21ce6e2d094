ca54ff4a24c0f4b47a217b1038a4b7112334e628
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-10-04 02:58:07 +0000

    Testing Sat Oct 04 2025 02:58:07 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex02/student/navigate.py
+++ chapter5/ex02/student/navigate.py
@@ -1,1 +1,21 @@
 # Write your program here
+inputFileName = input("Enter the input file name: ")
+
+inputFile = open(inputFileName, 'r')
+lines = []
+
+for line in inputFile:
+    lines.append(line)
+
+while True:
+    print("The file has ", len(lines), "lines.")
+    if len(lines) == 0:
+        break
+    lineNumber = int(input("Enter a line number [0 to quit]: "))
+
+    if lineNumber == 0:
+        break
+    elif lineNumber >= len(lines):
+        print("Error: line number must be less than", len(lines))
+    else:
+        print(lineNumber, ":", lines[lineNumber])
