1db065efc778d645eb898bea0a36d3ff1b4f248e
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-10-08 02:22:57 +0000

    Testing Wed Oct 08 2025 02:22:57 GMT+0000 (Coordinated Universal Time)

--- chapter5/dex01/student/toUpper.py
+++ chapter5/dex01/student/toUpper.py
@@ -1,4 +1,4 @@
 listOfWords = ["Apple", "orange", "banana"]
-for word in listOfWords:
-    word = word.upper()
+for i in range(len(listOfWords)):
+    listOfWords[i] = listOfWords[i].upper()
 print(listOfWords)
