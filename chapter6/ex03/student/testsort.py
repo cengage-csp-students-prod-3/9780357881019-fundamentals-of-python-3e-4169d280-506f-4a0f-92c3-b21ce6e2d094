6443ba65d333a6ccea6618e16e6d2962c3417dad
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-10-10 01:42:48 +0000

    Testing Fri Oct 10 2025 01:42:48 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex03/student/testsort.py
+++ chapter6/ex03/student/testsort.py
@@ -1,1 +1,23 @@
 # Write your code here
+def isSorted(sort_list):
+
+    if(len(sort_list) == 0 or len(sort_list) == 1):
+        return True
+    
+    else: 
+        for index in range(len(sort_list) - 1):
+            if sort_list[index] > sort_list[index + 1]:
+                return False
+            
+    return True
+
+def main():
+
+    list_1 = []
+    print(isSorted(list_1))
+
+    list_1 = [1]
+    print(isSorted(list_1))
+
+    list_1 = list(range(10))
+    print(isSorted(list_1))
