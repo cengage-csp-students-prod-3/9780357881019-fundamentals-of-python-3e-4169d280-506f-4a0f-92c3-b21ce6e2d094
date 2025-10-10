ded4970682158a1361eef515cf26af5b98c837f3
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-10-10 01:27:38 +0000

    Testing Fri Oct 10 2025 01:27:38 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex02/student/newton.py
+++ chapter6/ex02/student/newton.py
@@ -19,7 +19,7 @@ def limitReached(x, estimate):
     return difference <= TOLERANCE
 
 def improveEstimate(x, estimate):
-    return (estimate + x / estimate) / 2
+    return (estimate + x / estimate) 
 
 def main():
     while True:
