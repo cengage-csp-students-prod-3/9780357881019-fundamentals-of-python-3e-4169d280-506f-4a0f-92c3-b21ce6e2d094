3ce95c7160156438356b033253f6141e7e148005
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-10-17 02:18:11 +0000

    Testing Fri Oct 17 2025 02:18:11 GMT+0000 (Coordinated Universal Time)

--- chapter7/dex01/student/nounPhrase.py
+++ chapter7/dex01/student/nounPhrase.py
@@ -1,16 +1,26 @@
 import random
+
 articles = ["the", "a", "one", "some", "any"]
 adjectives = ["big", "small", "blue", "green", "yellow", "red", "purple", "orange", "brown", "black", "white"]
 nouns = ["apple", "peach", "banana", "pear", "orange", "grape", "strawberry", "blueberry", "raspberry", "cherry", "watermelon", "cantaloupe", "honeydew"]
-#nounphrase = article [adjectivephrase] noun
+
+# nounphrase = article [adjectivephrase] noun
 def nounPhrase():
     """Builds and returns a noun phrase."""
-    return random.choice(articles) + " " + \
-        adjectivePhrase() + " " + random.choice(nouns)
+    phrase = random.choice(articles) + " "
+    # Randomly decide whether to include adjectives
+    if random.choice([True, False]):
+        phrase += adjectivePhrase() + " "
+    phrase += random.choice(nouns)
+    return phrase
 
-#adjectivephrase = adjective [adjectivephrase]
+# adjectivephrase = adjective [adjectivephrase]
 def adjectivePhrase():
     """Builds and returns an adjective phrase."""
-    return random.choice(adjectives) + " " + adjectivePhrase()
+    phrase = random.choice(adjectives)
+    # Randomly decide whether to continue adding adjectives
+    if random.choice([True, False]):
+        phrase += " " + adjectivePhrase()
+    return phrase
 
 print(nounPhrase())
