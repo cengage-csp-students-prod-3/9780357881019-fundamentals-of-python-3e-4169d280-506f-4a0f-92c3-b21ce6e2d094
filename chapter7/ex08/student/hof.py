ff8db448e9b13f2e62cc134795c1cea09aca036d
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-10-17 02:09:42 +0000

    Testing Fri Oct 17 2025 02:09:42 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex08/student/hof.py
+++ chapter7/ex08/student/hof.py
@@ -1,1 +1,27 @@
 # Write your code here
+def myMap(func, lst):
+    """Applies func to each element of lst and returns a new list."""
+    result = []
+    for item in lst:
+        result.append(func(item))
+    return result
+
+
+def myFilter(func, lst):
+    """Returns a list of elements from lst for which func(item) is True."""
+    result = []
+    for item in lst:
+        if func(item):
+            result.append(item)
+    return result
+
+
+def myReduce(func, lst):
+    """Applies func cumulatively to the items of lst, reducing to a single value."""
+    if not lst:
+        raise TypeError("myReduce() of empty sequence with no initial value")
+    
+    result = lst[0]
+    for item in lst[1:]:
+        result = func(result, item)
+    return result
--- chapter7/ex08/student/testhof.py
+++ chapter7/ex08/student/testhof.py
@@ -1,1 +1,34 @@
 # Write your code here
+from hof import myMap, myFilter, myReduce
+from functools import reduce
+
+def square(x): return x ** 2
+def isOdd(x): return x % 2 == 1
+def add(x, y): return x + y
+
+test_lists = [
+    [0, 1, 2, 3, 4],
+    [3],
+    []
+]
+
+for lst in test_lists:
+    print("Argument list:  ", lst)
+
+    # --- map test ---
+    print("map with ** 2  :", list(map(square, lst)))
+    print("myMap with ** 2:", myMap(square, lst))
+
+    # --- filter test ---
+    print("filter with odd  :", list(filter(isOdd, lst)))
+    print("myFilter with odd:", myFilter(isOdd, lst))
+
+    # --- reduce test ---
+    if lst:
+        print("reduce with +   :", reduce(add, lst))
+        print("myReduce with + :", myReduce(add, lst))
+    else:
+        print("reduce with +   : []")
+        print("myReduce with + : []")
+
+    print()
