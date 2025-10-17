e54943e35aa3cd1d55fa2d824af2cf2ce4689690
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-10-17 01:11:31 +0000

    Testing Fri Oct 17 2025 01:11:31 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex02/student/newton.py
+++ chapter7/ex02/student/newton.py
@@ -1,1 +1,30 @@
 # Write your code here
+import math
+
+TOLERANCE = 0.000001
+
+def newton(x, estimate = 1):
+    if limitReached(x, estimate):
+        return estimate
+    else:
+        return newton(x, improveEstimate(x, estimate))
+    
+def limitReached(x, estimate):
+    difference = abs(x - estimate ** 2)
+    return difference <= TOLERANCE
+
+def improveEstimate(x, estimate):
+    return (estimate + x / estimate) / 2
+
+def main():
+    while True:
+        x = input("Enter a positive number or enter/return to quit: ")
+        if x == "":
+            break
+        x = float(x)
+
+        print("The program's estimate is ", newton(x))
+        print("Python's estimate is ", math.sqrt(x))
+
+if __name__ == "__main__":
+    main()
