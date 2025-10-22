933d6286248d79b066a9f7f1349d2a41dfc70cbf
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-10-22 03:45:40 +0000

    Testing Wed Oct 22 2025 03:45:40 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex07/student/invert.py
+++ chapter8/ex07/student/invert.py
@@ -1,1 +1,26 @@
 # Write your code here
+from images import Image
+
+def invert(image):
+    """Invert the colors in the given image."""
+    width = image.getWidth()
+    height = image.getHeight()
+
+    for y in range(height):
+        for x in range(width):
+            (r, g, b) = image.getPixel(x, y)
+            # Invert each color component
+            newR = 255 - r
+            newG = 255 - g
+            newB = 255 - b
+            image.setPixel(x, y, (newR, newG, newB))
+    return image
+
+def main():
+    filename = input("Enter the image file name: ")
+    image = Image(filename)
+    invert(image)
+    image.draw()
+
+if __name__ == "__main__":
+    main()
