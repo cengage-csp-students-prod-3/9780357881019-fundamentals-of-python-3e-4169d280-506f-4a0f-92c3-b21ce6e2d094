d75a997ea3b34b8849621084f08946919f95168c
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-10-22 03:38:15 +0000

    Testing Wed Oct 22 2025 03:38:15 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex06/student/grayscale.py
+++ chapter8/ex06/student/grayscale.py
@@ -1,1 +1,24 @@
 # Write your code here
+from images import Image
+
+def grayscale(image):
+    """Convert image to grayscale by averaging RGB values."""
+    width = image.getWidth()
+    height = image.getHeight()
+
+    for y in range(height):
+        for x in range(width):
+            (r, g, b) = image.getPixel(x, y)
+            # Simple average method
+            avg = (r + g + b) // 3
+            image.setPixel(x, y, (avg, avg, avg))
+    return image
+
+def main():
+    filename = input("Enter the image file name: ")
+    image = Image(filename)
+    grayscale(image)
+    image.draw()
+
+if __name__ == "__main__":
+    main()
