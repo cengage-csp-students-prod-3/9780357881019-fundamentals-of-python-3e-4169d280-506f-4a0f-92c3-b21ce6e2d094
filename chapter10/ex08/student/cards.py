c9bf0c28f4643564c1ef1206db45e5f6488f3c57
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-11-07 03:41:35 +0000

    Testing Fri Nov 07 2025 03:41:35 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex08/student/cards.py
+++ chapter10/ex08/student/cards.py
@@ -1,22 +1,21 @@
 """
 File: cards.py
-
 Module for playing cards, with classes Card and Deck
 """ 
 import random
 
 class Card(object):
-    """ A card object with a suit and rank."""
+    """A card object with a suit and rank."""
 
     RANKS = tuple(range(1, 14))
-
     SUITS = ("Spades", "Diamonds", "Hearts", "Clubs")
 
     def __init__(self, rank, suit):
         """Creates a card with the given rank and suit."""
         self.rank = rank
         self.suit = suit
-        
+        self.faceup = False  # Task 1: add faceup instance variable
+
     def __str__(self):
         """Returns the string representation of a card."""
         if self.rank == 1:
@@ -29,12 +28,15 @@ class Card(object):
             rank = "King"
         else:
             rank = self.rank
-        return str(rank) + " of " + self.suit
+        return f"{rank} of {self.suit} {self.faceup}"
 
-import random
+    def turn(self):
+        """Task 2: Turn the card over (flip faceup status)."""
+        self.faceup = not self.faceup
+
 
 class Deck(object):
-    """ A deck containing 52 cards."""
+    """A deck containing 52 cards."""
 
     def __init__(self):
         """Creates a full deck of cards."""
@@ -49,36 +51,38 @@ class Deck(object):
         random.shuffle(self.cards)
 
     def deal(self):
-        """Removes and returns the top card or None 
-        if the deck is empty."""
+        """Removes and returns the top card or None if the deck is empty."""
         if len(self) == 0:
-           return None
+            return None
         else:
-           return self.cards.pop(0)
+            return self.cards.pop(0)
 
     def __len__(self):
-       """Returns the number of cards left in the deck."""
-       return len(self.cards)
+        """Returns the number of cards left in the deck."""
+        return len(self.cards)
 
     def __str__(self): 
         """Returns the string representation of a deck."""
         result = ''
         for c in self.cards:
-            result = self.result + str(c) + '\n'
+            result = result + str(c) + '\n'
         return result
+
 
 def main():
     """A simple test."""
     deck = Deck()
-    print("A new deck:")
-    while len(deck) > 0:
-        print(deck.deal())
-    deck = Deck()
+    print("A new deck, cards face down:")
+    print(deck)
+
+    # Turn all cards face up
+    for card in deck.cards:
+        card.turn()
+
     deck.shuffle()
-    print("A deck shuffled once:")
-    while len(deck) > 0:
-        print(deck.deal())
+    print("A deck shuffled once, cards face up:")
+    print(deck)
+
 
 if __name__ == "__main__":
     main()
-    
