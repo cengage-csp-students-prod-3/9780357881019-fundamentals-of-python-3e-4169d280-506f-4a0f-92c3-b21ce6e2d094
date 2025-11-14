7cb8692e655555107f2ca67b825d4951b3518063
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-11-14 00:55:49 +0000

    Testing Fri Nov 14 2025 00:55:49 GMT+0000 (Coordinated Universal Time)

--- chapter11/ex01/student/studentview.py
+++ chapter11/ex01/student/studentview.py
@@ -16,49 +16,57 @@ class StudentView(EasyFrame):
         self.addLabel("Mean", row = 0, column = 0)            
         self.addLabel("Median", row = 1, column = 0)            
         self.addLabel("Mode", row = 2, column = 0)            
-        self.addLabel("Standard deviation", row = 3, 
-                                            column = 0)
+        self.addLabel("Standard deviation", row = 3, column = 0)
+
         self.meanFld = self.addFloatField(value = 0.0, 
                                           row = 0,
                                           column = 1,
-                                          precision = 2)            
+                                          precision = 2)
+
         self.medianFld = self.addFloatField(value = 0.0, 
                                             row = 1,
                                             column = 1,
-                                            precision = 2)            
+                                            precision = 2)
+
         self.modeFld = self.addFloatField(value = 0.0, 
                                           row = 2,
                                           column = 1,
-                                          precision = 1)            
+                                          precision = 1)
+
         self.stdFld = self.addFloatField(value = 0.0, 
                                          row = 3,
                                          column = 1,
-                                         precision = 4)                        
-        self.addLabel("Data", row = 0, column = 2,
-                      sticky = "NEW")
-        self.scoreArea = self.addTextArea(text = "", 
+                                         precision = 4)
+
+        self.addLabel("Data", row = 0, column = 2, sticky = "NEW")
+        self.scoreArea = self.addTextArea(text="",
                                           row = 1,
                                           column = 2,
                                           width = 12,
                                           rowspan = 3)
-        # Create a panel for the buttons to center them
-        # in 4 columns
+
+        # Panel for buttons
         bp = self.addPanel(row = 4, column = 0,
                            columnspan = 3,
                            background = "black")
+
         bp.addButton(text = "Edit score", 
                      row = 0, column = 0,
                      command = self.editScore)
+
         bp.addButton(text = "Add score", 
                      row = 0, column = 1,
                      command = self.addScore)
+
         bp.addButton(text = "Delete score", 
                      row = 0, column = 2,
                      command = self.deleteScore)
+
         bp.addButton(text = "Randomize scores", 
                      row = 0, column = 3,
                      command = self.randomizeScores)
-        # Place the model's contents in the view
+
+        # Load model into the view
         self.refreshData()
 
     def refreshData(self):
@@ -70,29 +78,68 @@ class StudentView(EasyFrame):
         self.stdFld.setNumber(self.model.getStd())
         self.scoreArea.setText(str(self.model))
 
+    # -------------------------------------------------------
     # Event-handling methods
+    # -------------------------------------------------------
 
     def editScore(self):
         """Obtains a new score and its position from the user
         and updates the model and the view."""
-        return
+        pos = self.prompterBox(
+            title="Edit score",
+            promptString="Position of the score to change:"
+        )
+        score = self.prompterBox(
+            title="Edit score",
+            promptString="New score:"
+        )
+
+        if pos is not None and score is not None:
+            self.model.setScore(int(pos), int(score))
+            self.refreshData()
 
     def addScore(self):
         """Obtains a new score from the user,
         adds it to the model, and updates the view."""
-        return
-    
+        score = self.prompterBox(
+            title="Add score",
+            promptString="New score:"
+        )
+        if score is not None:
+            self.model.addScore(int(score))
+            self.refreshData()
+
     def deleteScore(self):
         """Obtains the position of a score from the user,
         deletes the score at that position from the model,
         and updates the view."""
-        position = self.prompterBox(title = "Delete score",
-                                    promptString = "Position of the score:")
-        self.model.deleteScore(int(position))
-        self.refreshData()
+        position = self.prompterBox(
+            title="Delete score",
+            promptString="Position of the score:"
+        )
+        if position is not None:
+            self.model.deleteScore(int(position))
+            self.refreshData()
     
     def randomizeScores(self):
-        """Obtains the number of scores, lowest score,
-        and highest score from the user, randomizes the model's scores,
-        and updates the view."""
-        return
+        """Gets number of scores, min, and max, randomizes them, updates view."""
+        count = self.prompterBox(
+            title="Randomize scores",
+            promptString="How many scores?"
+        )
+        low = self.prompterBox(
+            title="Randomize scores",
+            promptString="Lowest score:"
+        )
+        high = self.prompterBox(
+            title="Randomize scores",
+            promptString="Highest score:"
+        )
+
+        if count and low and high:
+            self.model.randomizeScores(
+                int(count),
+                int(low),
+                int(high)
+            )
+            self.refreshData()
