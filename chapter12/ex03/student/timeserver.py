1ac68cb32d878aa335034dbbfa0bf68bbd324e35
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-11-21 02:40:33 +0000

    Testing Fri Nov 21 2025 02:40:33 GMT+0000 (Coordinated Universal Time)

--- chapter12/ex03/student/timeserver.py
+++ chapter12/ex03/student/timeserver.py
@@ -1,27 +1,72 @@
 """
 File: timeserver.py
-Programming Exercise 12.2
+Programming Exercise 12.2 (Modified)
 
-Server for providing the day and time.  Uses client
-handlers to handle clients' requests.
+Server for providing the day and time. Allows the server user
+to press ENTER to shut the server down. Runs on its own thread.
 """
 
 from socket import *
 from timeclienthandler import TimeClientHandler
+from threading import Thread
 
 HOST = "localhost"
 PORT = 6000
 ADDRESS = (HOST, PORT)
 
-server = socket(AF_INET, SOCK_STREAM)
-server.bind(ADDRESS)
-server.listen(5)
 
-# The server now just waits for connections from clients
-# and hands sockets off to client handlers
-while True:
-    print("Waiting for connection . . .")
-    client, address = server.accept()
-    print("... connected from: ", address)
-    handler = TimeClientHandler(client)
-    handler.start()
+class TimeServer(Thread):
+    """Day/Time server that runs until shutdown signal is given."""
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.server = socket(AF_INET, SOCK_STREAM)
+        self.server.bind(ADDRESS)
+        self.server.listen(5)
+        self.running = True
+
+    def run(self):
+        """Main server loop, accepting clients until shutdown."""
+        print("Waiting for connection . . .")
+
+        # Keep running until quit() shuts down the socket
+        while self.running:
+            try:
+                self.server.settimeout(1)  # prevents blocking forever
+                client, address = self.server.accept()
+            except timeout:
+                continue
+            except OSError:
+                break  # Occurs after shutdown
+
+            print("... connected from: ", address)
+            handler = TimeClientHandler(client)
+            handler.start()
+
+        try:
+            self.server.close()
+        except:
+            pass
+
+    def quit(self):
+        """Shutdown server loop and close socket."""
+        self.running = False
+        try:
+            self.server.close()
+        except:
+            pass
+
+
+def main():
+    """Starts the server thread and waits for user input to shut down."""
+    server = TimeServer()
+    server.start()
+    print("Press enter to shut the server down.")
+
+    input()  # waits for shutdown command
+    server.quit()
+    print("Server shutting down.")
+
+
+if __name__ == "__main__":
+    main()
