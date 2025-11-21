0d93306e7584ededa0a05c20178522a06b92a511
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-11-21 01:20:20 +0000

    Testing Fri Nov 21 2025 01:20:20 GMT+0000 (Coordinated Universal Time)

--- chapter12/ex03/student/timeserver.py
+++ chapter12/ex03/student/timeserver.py
@@ -1,27 +1,58 @@
 """
 File: timeserver.py
-Programming Exercise 12.2
+Modified Programming Exercise 12.2
 
-Server for providing the day and time.  Uses client
-handlers to handle clients' requests.
+Time server that allows shutting down by pressing ENTER.
 """
 
 from socket import *
+from threading import Thread
+from time import ctime
 from timeclienthandler import TimeClientHandler
 
-HOST = "localhost"
-PORT = 6000
-ADDRESS = (HOST, PORT)
 
-server = socket(AF_INET, SOCK_STREAM)
-server.bind(ADDRESS)
-server.listen(5)
+class TimeServer(Thread):
 
-# The server now just waits for connections from clients
-# and hands sockets off to client handlers
-while True:
-    print("Waiting for connection . . .")
-    client, address = server.accept()
-    print("... connected from: ", address)
-    handler = TimeClientHandler(client)
-    handler.start()
+    def __init__(self, port=50000):
+        Thread.__init__(self)
+        self.host = ""
+        self.port = port
+        self.serverSocket = socket(AF_INET, SOCK_STREAM)
+        self.serverSocket.bind((self.host, self.port))
+        self.serverSocket.listen(5)
+
+        self.running = True  # server control flag
+
+    def run(self):
+        print("Waiting for connection . . .")
+        while self.running:
+            self.serverSocket.settimeout(1)  # prevents blocking during shutdown check
+            try:
+                client, address = self.serverSocket.accept()
+                print("... connected from:", address)
+                handler = TimeClientHandler(client)
+                handler.start()
+            except timeout:
+                continue  # loop again to check if shutting down
+
+        self.serverSocket.close()
+        print("Server shut down.")
+
+    def quit(self):
+        """Signals the server loop to stop."""
+        self.running = False
+
+    @staticmethod
+    def main():
+        server = TimeServer()
+        server.start()  # start server thread
+
+        input("Press enter to shut the server down.\n")
+        server.quit()
+
+        print("Server shutting down.")
+        server.join()
+
+
+if __name__ == "__main__":
+    TimeServer.main()
