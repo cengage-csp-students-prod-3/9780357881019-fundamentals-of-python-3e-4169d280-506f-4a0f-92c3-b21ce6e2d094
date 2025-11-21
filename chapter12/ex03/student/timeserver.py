201d847700815649813810b815ccc0e9541309ab
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-11-21 02:36:27 +0000

    Testing Fri Nov 21 2025 02:36:27 GMT+0000 (Coordinated Universal Time)

--- chapter12/ex03/student/timeserver.py
+++ chapter12/ex03/student/timeserver.py
@@ -1,58 +1,72 @@
 """
 File: timeserver.py
-Modified Programming Exercise 12.2
+Programming Exercise 12.2 Modified
 
-Time server that allows shutting down by pressing ENTER.
+Allows server-side shutdown by pressing Enter.
 """
 
 from socket import *
 from threading import Thread
 from time import ctime
-from timeclienthandler import TimeClientHandler
+
+HOST = ''
+PORT = 50007
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
 
 
 class TimeServer(Thread):
+    """Day/Time Server allowing manual shutdown."""
 
-    def __init__(self, port=50000):
+    def __init__(self):
         Thread.__init__(self)
-        self.host = ""
-        self.port = port
-        self.serverSocket = socket(AF_INET, SOCK_STREAM)
-        self.serverSocket.bind((self.host, self.port))
-        self.serverSocket.listen(5)
-
-        self.running = True  # server control flag
+        self.server = socket(AF_INET, SOCK_STREAM)
+        self.server.bind(ADDR)
+        self.server.listen(5)
+        self.running = True
 
     def run(self):
+        """Wait continuously for client connections until shutdown."""
         print("Waiting for connection . . .")
-        while self.running:
-            self.serverSocket.settimeout(1)  # prevents blocking during shutdown check
-            try:
-                client, address = self.serverSocket.accept()
-                print("... connected from:", address)
-                handler = TimeClientHandler(client)
-                handler.start()
-            except timeout:
-                continue  # loop again to check if shutting down
 
-        self.serverSocket.close()
-        print("Server shut down.")
+        while self.running:
+            try:
+                self.server.settimeout(1)  # Allows periodic checking if shutting down
+                client, address = self.server.accept()
+            except timeout:
+                continue  # Check shutdown flag again
+            except OSError:
+                break  # socket closed externally, shutdown complete
+
+            print("... connected from:", address)
+            client.send(bytes(ctime() + "\nHave a nice day!", "utf-8"))
+            client.close()
+
+        try:
+            self.server.close()
+        except:
+            pass
 
     def quit(self):
-        """Signals the server loop to stop."""
+        """Stops server loop."""
         self.running = False
+        try:
+            self.server.close()
+        except:
+            pass
 
-    @staticmethod
-    def main():
-        server = TimeServer()
-        server.start()  # start server thread
 
-        input("Press enter to shut the server down.\n")
-        server.quit()
+def main():
+    """Main thread allowing server shutdown from keyboard."""
+    server = TimeServer()
+    server.start()
+    print("Press enter to shut the server down.")
 
-        print("Server shutting down.")
-        server.join()
+    # User presses Enter → shutdown
+    input()
+    server.quit()
+    print("Server shutting down.")
 
 
 if __name__ == "__main__":
-    TimeServer.main()
+    main()
