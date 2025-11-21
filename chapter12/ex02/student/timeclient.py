66f2c46c3023e435be1b185ae06ba4429df5e449
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-11-21 01:04:43 +0000

    Testing Fri Nov 21 2025 01:04:43 GMT+0000 (Coordinated Universal Time)

--- chapter12/ex02/student/timeclient.py
+++ chapter12/ex02/student/timeclient.py
@@ -1,24 +1,21 @@
-"""
-File: timeclient.py
-Programming Exercise 12.2
-
-Client for obtaining the day and time. Recovers from connection
-errors.
-"""
-
 from socket import *
-from codecs import decode
+from time import ctime
 
-HOST = "localhost" 
-PORT = 6000
+HOST = 'localhost'
+PORT = 21567
 BUFSIZE = 1024
-ADDRESS = (HOST, PORT)
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
 
 try:
-    server = socket(AF_INET, SOCK_STREAM)               # Create a socket
-    server.connect(ADDRESS)                             # Connect it to a host
-    dayAndTime = decode(server.recv(BUFSIZE), "ascii")  # Read a string from it
-    print(dayAndTime)
-    server.close()                                      # Close the connection
+    tcpCliSock.connect(ADDR)
 except ConnectionRefusedError:
-    print("Error connecting to the server.")
+    print("Error connecting to the server")
+    exit()
+
+data = tcpCliSock.recv(BUFSIZE)
+print(data.decode())
+print("Have a nice day!")
+
+tcpCliSock.close()
