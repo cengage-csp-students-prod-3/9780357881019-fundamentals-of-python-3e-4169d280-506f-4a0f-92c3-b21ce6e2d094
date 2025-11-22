e0b5ccdc0e75aa3fb5ccab848f0cf9278a19c6a9
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-11-22 16:13:08 +0000

    Testing Sat Nov 22 2025 16:13:08 GMT+0000 (Coordinated Universal Time)

--- chapter12/dex01/student/server.py
+++ chapter12/dex01/student/server.py
@@ -3,19 +3,15 @@ File: server.py
 """
 from socket import *
 from codecs import decode
+
 HOST = "localhost"
-PORT = 6000
+PORT = 5000         # Changed to match client
 ADDRESS = (HOST, PORT)
+
 server = socket(AF_INET, SOCK_STREAM)
 server.bind(ADDRESS)
-"""
-File: client.py
-"""
-from socket import *
-from codecs import decode
-HOST = "localhost"
-PORT = 5000
-BUFSIZE = 1024
-ADDRESS = (HOST, PORT)
-server = socket(AF_INET, SOCK_STREAM) # Create a socket
-server.connect(ADDRESS) # Connect it to a host
+server.listen(1)
+
+print("Server ready and waiting for a connection...")
+client, addr = server.accept()
+print("Connected to:", addr)
