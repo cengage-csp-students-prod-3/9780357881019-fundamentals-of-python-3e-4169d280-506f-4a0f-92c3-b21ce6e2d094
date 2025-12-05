611e7216f9cd95952159e7636cd4ee3fae4abc00
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-12-05 23:56:04 +0000

    Testing Fri Dec 05 2025 23:56:04 GMT+0000 (Coordinated Universal Time)

--- chapter13/ex02/student/reverse.py
+++ chapter13/ex02/student/reverse.py
@@ -1,1 +1,12 @@
 # Write your code here
+
+def reverse(lyst):
+    """Reverses the elements of lyst in place without using list.reverse()."""
+    left = 0
+    right = len(lyst) - 1
+
+    # Swap mirror elements until the middle is reached
+    while left < right:
+        lyst[left], lyst[right] = lyst[right], lyst[left]
+        left += 1
+        right -= 1
