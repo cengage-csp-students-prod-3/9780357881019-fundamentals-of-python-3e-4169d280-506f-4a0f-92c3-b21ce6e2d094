c184bcc357c222c964412ce2ba7dc9f32466cc09
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-12-06 01:02:01 +0000

    Testing Sat Dec 06 2025 01:02:01 GMT+0000 (Coordinated Universal Time)

--- chapter13/ex06/student/fib.py
+++ chapter13/ex06/student/fib.py
@@ -15,31 +15,22 @@ class Counter:
 def fib(n):
     """Returns the nth Fibonacci number using memoization."""
 
-    memo = {}            # dictionary for memoized values
-    counter = Counter()  # count recursive calls
+    memo = {}
+    counter = Counter()
 
     def helper(k):
         counter.increment()
 
-        # Base cases
         if k <= 1:
             return k
 
-        # Check memo
         value = memo.get(k)
         if value is not None:
             return value
 
-        # Recursive computation
         value = helper(k - 1) + helper(k - 2)
-
-        # Store in memo
         memo[k] = value
         return value
 
     result = helper(n)
-
-    # Print number of recursive calls (as required by directions)
-    print(counter.count)
-
-    return result
+    return result   # <-- No printing!
