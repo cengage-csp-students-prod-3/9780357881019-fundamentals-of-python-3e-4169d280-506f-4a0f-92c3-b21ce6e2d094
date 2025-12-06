09e6f776aa055f9fdaacf912e6051da8be26d078
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-12-06 00:07:31 +0000

    Testing Sat Dec 06 2025 00:07:31 GMT+0000 (Coordinated Universal Time)

--- chapter13/ex03/student/expo.py
+++ chapter13/ex03/student/expo.py
@@ -1,1 +1,8 @@
 # Write your code here
+
+def expo(number, exponent):
+    """Returns number raised to the given nonnegative exponent."""
+    result = 1
+    for _ in range(exponent):
+        result *= number
+    return result
