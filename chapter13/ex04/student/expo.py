91765704b278cccac28cc191c3b2e937e5160c53
domoniquesdp <ddelpielago@horizon.csueastbay.edu>
2025-12-06 00:23:38 +0000

    Testing Sat Dec 06 2025 00:23:38 GMT+0000 (Coordinated Universal Time)

--- chapter13/ex04/student/expo.py
+++ chapter13/ex04/student/expo.py
@@ -1,1 +1,17 @@
 # Write your code here
+
+def expo(number, exponent):
+    """Returns number raised to the given nonnegative exponent using
+    recursive exponentiation by squaring."""
+    
+    # Base case
+    if exponent == 0:
+        return 1
+    
+    # If exponent is odd
+    if exponent % 2 == 1:
+        return number * expo(number, exponent - 1)
+    
+    # If exponent is even
+    half = expo(number, exponent // 2)
+    return half * half
